1a1e536dc881e8f32db950facff3109554fc2bd7
Murugan Amirthalingam <murugan.itsofficial@gmail.com>
2026-03-13 01:55:20 +0530

    changes

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const connectDB = require('../backend/src/config/db');
 
 // Handle Vercel serverless environment
 module.exports = async (req, res) => {
+    console.log(`Incoming request: ${req.method} ${req.url}`);
     await connectDB();
     return app(req, res);
 };
